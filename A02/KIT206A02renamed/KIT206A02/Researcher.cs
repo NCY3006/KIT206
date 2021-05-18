ae84a6bc0f533893c6828b69541da4b77f03321e
NCY3006 <83627051+NCY3006@users.noreply.github.com>
2021-05-18 11:20:07 +1000

    Update Researcher.cs

--- A02/KIT206A02renamed/KIT206A02/Researcher.cs
+++ A02/KIT206A02renamed/KIT206A02/Researcher.cs
@@ -25,45 +25,19 @@ namespace RAP
         public string Unit { get; set; }
         public enum Campus { get; set; }
         public string Email { get; set; }
-        //public URL Photo { get; set; } ??
+        public string PhotoURL { get; set; } //public URL Photo { get; set; } ??
         public string Degree { get; set; }
         public int Supervisor_id { get; set; }
         public enum Level { get; set; }
         public DateTime Utas_start { get; set; }
         public DateTime Current_start { get; set; 
-                                       
-                                       
-        public List<Publication> Skills { get; set; }
-
-        public int SkillCount
-        {
-            get { return Skills == null ? 0 : Skills.Count(); }
-        }
-
-        //The SkillCount out of 10, expressed as a percentage
-        public double SkillPercent
-        {
-            //This is equivalent to SkillCount / 10.0 * 100
-            get { return SkillCount * 10.0; }
-        }
-
-        //This is likely the solution you will have devised
-        public DateTime MostRecentTraining
-        {
-            get
-            {
-                var skillDates = from Researcher s in Skills
-                                 orderby s.Certified descending
-                                 select s.Certified;
-                return skillDates.First();
-            }
-        }
-
-
-        public override string ToString()
-        {
-            //For the purposes of this week's demonstration this returns only the name
-            return Name;
-        }
+                                                                             
+        public List<Publication> Skill { get; set; }
+        
+        //calculate attributes
+        public string Current_start { get { return EnumString.Description(CurrentJob); } }
+        public double Tenure { get { return DateTime.Today.Subtract(EarliestStart).TotalDays / 365.2425; } }
     }
+}
+        
 }
