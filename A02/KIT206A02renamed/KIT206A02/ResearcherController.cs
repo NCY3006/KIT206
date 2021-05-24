1b554e22a9991b0adc13d8790fa748e4f68de329
NCY3006 <mcyng@utas.edu.au>
2021-05-24 13:23:57 +1000

    add

---
+++ A02/KIT206A02renamed/KIT206A02/ResearcherController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIT206A02
+{
+    class ResearcherController
+    {
+    }
+}
