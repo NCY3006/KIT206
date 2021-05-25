286b0affaa1b0e6931f29c045e6a91d539cda194
CrimePope <McC.arthy1@yahoo.com.au>
2021-05-25 11:18:35 +1000

    t

--- A02/KIT206A02renamed/KIT206A02/Position.cs
+++ A02/KIT206A02renamed/KIT206A02/Position.cs
@@ -3,26 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using RAP;
 
-namespace RAP
+
+namespace RAP.Research
 {
     
     public class Position
     {
-        public EmploymentLevel Level { get; set; }
+        public EmploymentLevel Level { get; set; }  
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
+        public List<Researcher> Type { get; set; }
 
-       
-        public string Title(Researcher)
+        //Title will return the persons title eg: "DR, Miss"
+        public string Title
         {
-             string personsTitle = 
-             from title in researcher
-             where Researcher = researcher
-             return personsTitle; 
-        }
+            get {
+                string personsTitle =
+                from Researcher in Type
+                where Type = researcher
+                return personsTitle;
 
+                 }
+        }
+        //ToTile will return the name of thier employment based on thier level
         public string ToTitle(EmploymentLevel level)
         {
             switch(level)
